dd4c9b81a5b4b6b1c915ffda34bb92a0c7617504
Sneha Gupta <snehagupta53361@gmail.com>
2026-06-30 21:03:07 +0530

    package.json updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,11 @@
-require("dotenv").config()
-const app = require("./src/app")
-const connectToDB = require("./src/config/database")
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
 
-connectToDB()
-
+connectToDB();
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+  console.log("Server is running on port 3000");
+});
