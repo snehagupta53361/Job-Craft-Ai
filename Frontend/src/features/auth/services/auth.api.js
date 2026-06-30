16897ab0951e9e4a6d79659e197b8cc7fe1959c8
Sneha Gupta <snehagupta53361@gmail.com>
2026-06-30 16:45:53 +0530

    initial updates

---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
+export async function register({ username, email, password }) {
+  try {
+    const response = await api.post("/api/auth/register", {
+      username,
+      email,
+      password,
+    });
+
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function login({ email, password }) {
+  try {
+    const response = await api.post("/api/auth/login", {
+      email,
+      password,
+    });
+
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function logout() {
+  try {
+    const response = await api.get("/api/auth/logout");
+
+    return response.data;
+  } catch (err) {}
+}
+
+export async function getMe() {
+  try {
+    const response = await api.get("/api/auth/get-me");
+
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
